14dca79f00dd9284ee612bbed713f3a41d5d19e5
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-10-20 12:26:04 -0500

    Create sqlQueries.js

---
+++ scripts/sqlQueries.js
@@ -0,0 +1,38 @@
+const connection = require("./dbConnection");
+
+function sqlQueries(option) {
+
+    if (option.choice === "Add Departments") {
+        connection.query("INSERT INTO department SET ?",
+            [{
+                name: option.deptName
+            }], function (error, response) {
+                if (error) { throw error };
+            })
+    } else if (option.choice === "Add Roles") {
+        connection.query("INSERT INTO role SET ?",
+            [{
+                title: option.title,
+                salary: option.salary,
+                department_id: option.departmentId
+            }],
+            function (error, response) {
+                if (error) { throw error };
+            })
+    } else if (option.choice === "Add Employees") {
+
+    } else if (option.choice === "View Departments") {
+
+    } else if (option.choice === "View Roles") {
+
+    } else if (option.choice === "View Employees") {
+
+    } else if (option.choice === "Update Employee Roles") {
+
+    } else {
+        console.log("Error, Exiting");
+        connection.end();
+    }
+}
+
+module.exports = sqlQueries;
